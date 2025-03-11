fc972574eaac9fba6ec753605efa515d65981487
Vilidiana Draganova <vilidiana.draganova@p4future.com>
2025-03-11 08:43:45 +0100

    added application for sonarqube testing

---
+++ MainProcessWithUnitTestDemo/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/MainProcessWithUnitTestDemo/Tests/MainProcessWithProcessStarterMocked.bwt#/"/>
+  <EmulationDataFile>ACED00057400274D61696E50726F636573735769746850726F63657373537461727465724D6F636B65642E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001C526573745365727669636542696E64696E674D6F636B65642E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574001C536F61705365727669636542696E64696E674D6F636B65642E627774</EmulationDataFile>
+</emulation:BWTFile>
